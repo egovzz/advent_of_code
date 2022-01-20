4bf7cb68a18e8f95f60c050f5b4d337b5e6c57ef
Bryan Vazquez <bryan.vazquez@lernit.com.mx>
2022-01-19 23:48:39 -0600

    day3 complete

--- 2021/day3/binary_diagnostic.js
+++ 2021/day3/binary_diagnostic.js
@@ -52,11 +52,77 @@ const part1 = () => {
   });
 };
 
-const part2 = () => {};
-
-part2();
+part1();
 // parseInt with radix (second value) 2 is to easily convert binary number
 console.log('gamma: ', gamma, ' - ', parseInt(gamma, 2));
 console.log('epsilon: ', epsilon, ' - ', parseInt(epsilon, 2));
 
 console.log('Result: ', parseInt(gamma, 2) * parseInt(epsilon, 2));
+
+let oxygen = '';
+let co2 = '';
+const part2 = () => {
+  const findOxygen = (arrayToEvaluate, position = 0) => {
+    const ones = [];
+    const ceros = [];
+
+    arrayToEvaluate.forEach((element) => {
+      const number = element[position];
+      if (number === '1') {
+        ones.push(element);
+      } else {
+        ceros.push(element);
+      }
+    });
+
+    if (
+      position === arrayToEvaluate[0].length ||
+      arrayToEvaluate.length === 1
+    ) {
+      return arrayToEvaluate[0];
+    } else {
+      if (ones.length > ceros.length || ones.length === ceros.length) {
+        return findOxygen(ones, position + 1);
+      } else {
+        return findOxygen(ceros, position + 1);
+      }
+    }
+  };
+
+  const findCo2 = (arrayToEvaluate, position = 0) => {
+    const ones = [];
+    const ceros = [];
+
+    arrayToEvaluate.forEach((element) => {
+      const number = element[position];
+      if (number === '1') {
+        ones.push(element);
+      } else {
+        ceros.push(element);
+      }
+    });
+
+    if (
+      position === arrayToEvaluate[0].length ||
+      arrayToEvaluate.length === 1
+    ) {
+      return arrayToEvaluate[0];
+    } else {
+      if (ones.length > ceros.length || ones.length === ceros.length) {
+        return findCo2(ceros, position + 1);
+      } else {
+        return findCo2(ones, position + 1);
+      }
+    }
+  };
+
+  oxygen = findOxygen(input);
+  co2 = findCo2(input);
+};
+
+part2();
+// parseInt with radix (second value) 2 is to easily convert binary number
+console.log('oxygen: ', oxygen, ' - ', parseInt(oxygen, 2));
+console.log('co2: ', co2, ' - ', parseInt(co2, 2));
+
+console.log('Result: ', parseInt(oxygen, 2) * parseInt(co2, 2));
